f79447ce14d78e75ef025456530dedea62cad99f
michael <mgreenwaldt@fullsail.edu>
2013-06-15 22:10:27 -0700

    Second function created

--- JavaScript-3.js
+++ JavaScript-3.js
@@ -1,30 +1,46 @@
 //alert("JavaScript works!");
-var barleyBundle= {
+var barleyBundle = {
         name: "Barley Bundle",
         stock: "100",
         harvested: true,
-        }
+        };
     //end object
 var hotAir = 160;
 var hotWater = 100;
+var currentAir= 80;
 var dryTime = "36 hours";
 var steepTime = "1 hour";
 var hops = true
 var spices = true
 var yeast = ["ale yeast", "lager yeast"];
-var gather = barleyBundle.stock
+var barleyStock = barleyBundle.stock;
 var grain = barleyBundle.name;
 
 //function
-var maltPrep = function (barleyBundle, gather){
+var maltPrep = function (barleyStock) {
     var malt;
-    if (barleyBundle > gather) {
+    if (barleyStock > 50) {
+        ("There is enough" + grain + "to make malt.")
         malt = true
-        ("There is enough" + grain + "to make malt.");
     } else {
+        ("There is not enough " + grain + " to make malt.")
         malt = false
-        ("There is not enough " + grain + " to make malt.");
-    } return malt
+    }; return malt
 }
 //end function
-var gather = maltPrep();
+
+//new function start
+var dry = function (c,h) {
+    var dryBarley;
+    var warmUp;
+    while (c < h) {
+       warmUp = c++;
+        dryBarley = ("The air temperature is " + warmUp + " degrees");
+    }  return dryBarley;
+}
+
+var gather = maltPrep(barleyStock);
+console.log (gather);
+var drying = dry(currentAir,hotAir);
+console.log (drying);
+
