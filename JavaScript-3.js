b89b34966919cae8566135e511e23e47b199fc52
michael <mgreenwaldt@fullsail.edu>
2013-06-20 19:52:23 -0700

    added flowchart to repository.

--- JavaScript-3.js
+++ JavaScript-3.js
@@ -11,17 +11,18 @@ var mashKey = "mash";
 var fermentKey = "fermentTime";
 var barleyStockKey = "inStock";
 var barleyAmountKey = "amount";
-//object 1
+var boilKey = "boilingWater";
+//object 1 - JSON Data
 var lagerBeer = {
-    type:"lager",
-    ingredients:["barley", "water", "lager yeast", "hops"],
-    malt:function (currentAir,hotAir) {
+    "type":"lager",
+    "ingredients":["barley", "water", "lager yeast", "hops"],
+    "malt":function (currentAir,hotAir) {
             var warmUp;
             while (currentAir <= hotAir) {
             warmUp = currentAir++;
             }  return warmUp;
     },
-    mash: function (steepTime) {
+    "mash": function (steepTime) {
             var wort;
             var countDown;
             while (steepTime >= 0) {
@@ -29,15 +30,16 @@ var lagerBeer = {
                 if (steepTime === 0) {
                 wort = "Wort";
                 }
-            } return wort;
+            };
+            return wort;
     },
-    fermentTime: 5,
-    allReady: function (){
+    "fermentTime": 5,
+    "allReady": function (){
         console.log ("The lager is done, lets drink!");
+    
     }
-}
+};
 //end object
-
 //object 2
 var aleBeer = {
 
@@ -62,32 +64,23 @@ var aleBeer = {
         return wort;
     },
     fermentTime: 3,
-    boil: function(hotWater){
-        var startBoil;
-        for (i = 0; i < hotWater; i++) {
-            if (i === hotWater[i]) {
-            startBoil = true;
-            }
-        }
-       return startBoil;
-    },
     allReady: function (){
     console.log ("The ale is done, lets drink!");
     }
 } ;  
 //end object
-
-//begin object
+//object 3
 var barleyBundle = {
     inStock: true,
     amount: 500
 };
 //end object
-
+var barleyReady = barleyBundle.inStock;
+var barleyAmount = barleyBundle.amount;
 var lagerIngredients = lagerBeer.ingredients;
 var aleIngredients = aleBeer.ingredients;
 var aleIngredientsAmount = aleIngredients.length;
-var lagerIngredientsAmount = lagerIngredients.length
+var lagerIngredientsAmount = lagerIngredients.length;
 var getLagerYeast = (lagerIngredients)[2];
 var getAleYeast = (aleIngredients)[2];
 var findAleYeast = function (aleIngredientsAmount) {
@@ -108,12 +101,24 @@ var findLagerYeast = function (lagerIngredientsAmount) {
     }        
 var aleYeastType = findAleYeast(aleIngredientsAmount);
 var lagerYeastType = findLagerYeast(lagerIngredientsAmount);
+var getInventory = function (barleyBundle) {
+    if (barleyBundle == barleyBundle) {
+       var inventory;
+       inventory = true;
+    }
+    else {
+        inventory = false;
+    }
+    return inventory;
+};
+var inventory = getInventory(barleyBundle);
 
 
 
-//main code
-//Accessors
-
+//begin accessors
+//console.log (inventory);
+//console.log(barleyAmount);
+//console.log (barleyReady);
 //console.log (lagerYeastType);
 //console.log (aleYeastType);
 //console.log(aleIngredients);
@@ -136,16 +141,19 @@ var lagerYeastType = findLagerYeast(lagerIngredientsAmount);
 //console.log(aleBeer.malt(currentAir, hotAir));
 //lagerBeer["allReady"]();
 //aleBeer["allReady"]();
+// end accessors
 
-
+//main code
+console.log("First step is to check the inventory of barley. The records show " + barleyAmount + " lbs of barley.");
+console.log("If it is " + inventory + " there is barley in inventory, then lets make some beer.");
 console.log("There are 2 main beer types, " + lagerBeer[beerKey] + " and " + aleBeer[beerKey]+ ".");
-console.log("There are " + lagerIngredientsAmount + " main ingredients," + lagerIngredients +".");
-console.log("for both lagers and ale barley is heated to " + lagerBeer.malt(currentAir, hotAir) + " degrees.");
+console.log("There are " + lagerIngredientsAmount + " main ingredients; " + lagerIngredients +".");
+console.log("For both lagers and ale, barley is heated to " + lagerBeer.malt(currentAir, hotAir) + " degrees.");
 console.log("This is called malting, and the next step is to steep the malt in water above " + hotWater + " degrees for " + steepTime + " minutes.");
 console.log("Once the malt is ready it is drained and the liquid drained is called " + lagerBeer.mash(steepTime));
 console.log("The " + lagerBeer.mash(steepTime) + " is boiled for an hour.");
-console.log("At this point the wort is cooled and fermented. For " + lagerBeer[beerKey] + " beers, the fermentation time is " + lagerBeer[fermentKey] + " weeks and " + getLagerYeast + " is added.");
-console.log("For " + aleBeer[beerKey] + " the fermentation time is " + aleBeer[fermentKey] + " weeks and " + getAleYeast + " is added.");
+console.log("At this point the wort is cooled and fermented. For " + lagerBeer[beerKey] + " beers " + getLagerYeast + " is added. The fermentation time is " + lagerBeer[fermentKey] + " weeks.");
+console.log("For " + aleBeer[beerKey] + ", " +  getAleYeast + " is added.The fermentation time is " + aleBeer[fermentKey] + " weeks.");
 console.log("At this point " );
 lagerBeer["allReady"]();
 console.log(" or ");
